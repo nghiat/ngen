cd6f9d57ba10113b22d60646eda42895a3ecc2ca
nghiat <trantuannghia95@gmail.com>
2022-10-09 18:24:42 +0200

    Update setup script to support dnf

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 base_dir=$(dirname "$0")
 root_dir=$base_dir/..
-git submodule update "$root_dir/assets"
-git submodule update "$root_dir/third_party/stb"
-git submodule update "$root_dir/third_party/vulkan/Vulkan-Headers"
+git submodule update --init "$root_dir/assets"
+git submodule update --init "$root_dir/third_party/stb"
+git submodule update --init "$root_dir/third_party/vulkan/Vulkan-Headers"
 
 if [ -x "$(command -v apt)" ]; then
   sudo apt install clang python-is-python3 ninja-build libx11-xcb-dev libxcb-keysyms1-dev libxkbcommon-x11-dev
+elif [ -x "$(command -v dnf)" ]; then
+  sudo dnf install clang ninja-build libX11-devel xcb-util-keysyms-devel libxkbcommon-x11-devel
 fi
