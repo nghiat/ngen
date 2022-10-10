50078bd5a2c9c0448a5de572e50bf1419c6a274e
nghiat <trantuannghia95@gmail.com>
2022-10-11 00:22:44 +0200

    Add validation layers to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,9 +6,11 @@ git submodule update --init "$root_dir/third_party/stb"
 git submodule update --init "$root_dir/third_party/vulkan/Vulkan-Headers"
 
 if [ -x "$(command -v apt)" ]; then
-  sudo apt install clang python-is-python3 ninja-build libx11-xcb-dev libxcb-keysyms1-dev libxkbcommon-x11-dev
+  sudo apt install clang python-is-python3 ninja-build libx11-xcb-dev libxcb-keysyms1-dev libxkbcommon-x11-dev vulkan-validationlayers
 elif [ -x "$(command -v dnf)" ]; then
-  sudo dnf install clang ninja-build libX11-devel xcb-util-keysyms-devel libxkbcommon-x11-devel
+  sudo dnf install clang ninja-build libX11-devel xcb-util-keysyms-devel libxkbcommon-x11-devel vulkan-validation-layers
 elif [ -x "$(command -v pacman)" ]; then
-  sudo pacman -S clang ninja xcb-util-keysyms libxkbcommon-x11
+  sudo pacman -S clang ninja xcb-util-keysyms libxkbcommon-x11 vulkan-validation-layers
+else
+  echo "clang python-is-python3 ninja-build libx11-xcb-dev libxcb-keysyms1-dev libxkbcommon-x11-dev vulkan-validationlayers"
 fi
