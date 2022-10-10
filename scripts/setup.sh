4b4550cf0f617356c1019930a42921d18da7ce05
nghiat <trantuannghia95@gmail.com>
2022-10-10 21:37:59 +0200

    Add arch packages to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,6 @@ if [ -x "$(command -v apt)" ]; then
   sudo apt install clang python-is-python3 ninja-build libx11-xcb-dev libxcb-keysyms1-dev libxkbcommon-x11-dev
 elif [ -x "$(command -v dnf)" ]; then
   sudo dnf install clang ninja-build libX11-devel xcb-util-keysyms-devel libxkbcommon-x11-devel
+elif [ -x "$(command -v pacman)" ]; then
+  sudo pacman -S clang ninja xcb-util-keysyms libxkbcommon-x11
 fi
